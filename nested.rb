99b804373aaf66331c5927610fb64709b62bbed6
Tim <acronym1986@gmail.com>
2021-05-05 01:03:42 +1000

    done

--- nested.rb
+++ nested.rb
@@ -15,7 +15,10 @@ def hopper
         :languages => ["C"]
       }
     }
-
+##### solution:
+#programmer_hash.delete_if {|key| [:alan_kay , :dennis_ritchie].include?(key) } 
+programmer_hash.slice(0)
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +39,11 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+###### solution 
+  #programmer_hash.delete_if {|key| [:grace_hopper , :dennis_ritchie].include?(key)} 
+  #programmer_hash[:alan_kay].tap { |hs| hs.delete(:languages) }
+  programmer_hash.slice(1)
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +62,11 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+##### solution 
+#programmer_hash.delete_if {|key| [:grace_hopper , :alan_kay].include?(key)} 
+#programmer_hash[:dennis_ritchie].tap { |hs| hs.delete(:known_for) }
+programmer_hash.slice(2)
+programmer_hash[:dennis_ritchie][:languages][0].to_s
 end
 
 def adding_matz
@@ -81,7 +92,14 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    ####### solution 
+    programmer_hash.merge!(
+      :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+       }
+    )
+    programmer_hash
 end
 
 def changing_alan
@@ -103,7 +121,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    ###### solution 
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
+     
 end
 
 def adding_to_dennis
@@ -126,4 +147,7 @@ def adding_to_dennis
       }
     }
 
+    ###### solution 
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+    programmer_hash
 end
